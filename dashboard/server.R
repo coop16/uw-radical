d89187c3e4594241ba4fd5cce5c519854eb08618
Logan Piepmeier <loganap@DEOHS.WASHINGTON.EDU>
2017-03-03 10:25:03 -0800

    changes to dashboard

--- dashboard/server.R
+++ dashboard/server.R
@@ -2,21 +2,23 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
-setwd('~/Documents/Work/uw-radical')
-source('getMESA_data.R')
+setwd('H:/projects/uw-radical')
+source('getMESA_data_Kairos.R')
 
 shinyServer(function(input, output) {
-
+  site_lookup = list(
+      'NYC' = c('MESA9', 'MESA10', 'MESA11', 'MESA13', 'MESA14', 'MESA16')
+      )
   dataset <- reactivePoll(30 * 1000, NULL, # check every 30 seconds whether it's time for an update
     checkFunc = function(){floor_date(Sys.time() - 30, unit = '5 mins')}, # server updates every 5 minutes on the 5's -- we'll wait 30 extra seconds to allow for clock diffs
-    valueFunc = function(){getMESA_data(start = Sys.time() - (8*60*60), stop = Sys.time())$long}
+    valueFunc = function(){getMESA_data(start = Sys.time() - (24*60*60), stop = Sys.time())$long %>% filter(monitor %in% site_lookup[[input$site]])}
     )
 
   # Timeseries plot
 
   output$tsplot <- renderPlot({
     ts.plt <- ggplot(
-        dataset()[which(dataset()$tags %in% input$y),]
+        dataset()[which(dataset()$tags %in% input$channel),]
       ) +
       geom_line(aes_string(x='date', y='as.numeric(value)', color = 'tags')) +
       geom_point(aes_string(x='date', y='as.numeric(value)', color = 'tags')) +
@@ -24,48 +26,50 @@ shinyServer(function(input, output) {
       theme_minimal() +
       xlab('time') +
       ylab('val') +
-      ggtitle(paste(input$y, 'as of', as.character(max(dataset()$date))))
+      xlim(Sys.time() - (input$starttime*60*60), Sys.time() - (input$stoptime*60*60)) +
+      ggtitle(paste(input$channel, 'as of', as.character(max(dataset()$date)))) +
+      theme(legend.position="bottom")
 
 
     print(ts.plt)
   }, height = 1000)
-  output$tlplot <- renderPlot({
-    # Traffic Light plot
-
-    last.24h <- dataset() %>%
-      filter(date > max(date) - (24*60*60)) %>%
-      filter(tags %in% c('Plantower1_pm2_5_mass', 'Plantower2_pm2_5_mass', 'RH_val', 'S1_val', 'S2_val', 'Temp_val', 'CO_sensor', 'NO_sensor', 'NO2_sensor', 'O3_sensor')) %>%
-      mutate(ok = is.na(value))
-    last.pass <- last.24h %>% filter(date == max(date))
-
-    last24h.ok <- last.24h %>% group_by(monitor) %>% summarize(ok.last24h = sum(ok) > 0)
-    last.pass.ok <- last.pass %>% group_by(monitor) %>% summarize(ok.lastpass = sum(ok) > 0)
-
-    status <- dataset() %>%
-      dplyr::select(monitor) %>%
-      distinct() %>%
-      left_join(last24h.ok) %>%
-      left_join(last.pass.ok) %>%
-      mutate(
-        ok.last24h = ok.last24h & !is.na(ok.last24h),
-        ok.lastpass = ok.lastpass & !is.na(ok.lastpass)
-      ) %>%
-      transmute(
-        monitor,
-        status = factor(ok.last24h + ok.lastpass)
-      ) %>%
-      arrange(monitor)
-
-    tl.plt <- ggplot(status) +
-      geom_point(aes(x = '', y = monitor), color = 'black', size = 10) +
-      geom_point(aes(x = '', y = monitor, color = status), size = 8) +
-      xlab('') +
-      theme_minimal() +
-      scale_color_manual(values = c('red', 'yellow', 'green'), guide = F)
-
-    print(tl.plt)
-
-  })
+  # output$tlplot <- renderPlot({
+  #   # Traffic Light plot
+  #
+  #   last.24h <- dataset() %>%
+  #     filter(date > max(date) - (24*60*60)) %>%
+  #     filter(tags %in% c('Plantower1_pm2_5_mass', 'Plantower2_pm2_5_mass', 'RH_val', 'S1_val', 'S2_val', 'Temp_val', 'CO_sensor', 'NO_sensor', 'NO2_sensor', 'O3_sensor')) %>%
+  #     mutate(ok = is.na(value))
+  #   last.pass <- last.24h %>% filter(date == max(date))
+  #
+  #   last24h.ok <- last.24h %>% group_by(monitor) %>% summarize(ok.last24h = sum(ok) > 0)
+  #   last.pass.ok <- last.pass %>% group_by(monitor) %>% summarize(ok.lastpass = sum(ok) > 0)
+  #
+  #   status <- dataset() %>%
+  #     dplyr::select(monitor) %>%
+  #     distinct() %>%
+  #     left_join(last24h.ok) %>%
+  #     left_join(last.pass.ok) %>%
+  #     mutate(
+  #       ok.last24h = ok.last24h & !is.na(ok.last24h),
+  #       ok.lastpass = ok.lastpass & !is.na(ok.lastpass)
+  #     ) %>%
+  #     transmute(
+  #       monitor,
+  #       status = factor(ok.last24h + ok.lastpass)
+  #     ) %>%
+  #     arrange(monitor)
+  #
+  #   tl.plt <- ggplot(status) +
+  #     geom_point(aes(x = '', y = monitor), color = 'black', size = 10) +
+  #     geom_point(aes(x = '', y = monitor, color = status), size = 8) +
+  #     xlab('') +
+  #     theme_minimal() +
+  #     scale_color_manual(values = c('red', 'yellow', 'green'), guide = F)
+  #
+  #   print(tl.plt)
+  #
+  # })
   renderText
 
 })
--- dashboard/ui.R
+++ dashboard/ui.R
@@ -6,12 +6,25 @@ shinyUI(pageWithSidebar(
   headerPanel("Radical Dashboard"),
 
   sidebarPanel(
-    checkboxGroupInput('y',
+    checkboxGroupInput('channel',
       'Channel',
       c('Plantower1_pm2_5_mass', 'Plantower2_pm2_5_mass', 'RH_val', 'S1_val', 'S2_val', 'Temp_val', 'CO_sensor', 'NO_sensor', 'NO2_sensor', 'O3_sensor'),
       selected = 'Plantower1_pm2_5_mass'
       ),
-      plotOutput('tlplot')
+    sliderInput('starttime',
+      'Start time (hours ago)',
+      min = 1, max = 24, value = 24, step = 6
+      ),
+    sliderInput('stoptime',
+      'Stop time (hours ago)',
+      min = 0, max = 24, value = 0, step = 6
+      ),
+    selectInput('site',
+        'Site',
+        c('NYC'),
+        selected = 'NYC'
+        )
+      #,plotOutput('tlplot')
     ),
 
   mainPanel(
